85dedc41bd94c6341f88e930d681572498f3de48
yash-kh <yashkhatri2198@gmail.com>
2024-12-22 09:19:31 +0530

    get started added

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,34 @@ import {
   watchSeries,
   watchSize,
 } from "@/modals/watch";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ButtonList from "@/components/ButtonList/ButtonList";
 import Dropdown from "@/components/Dropdown/Dropdown";
 import Image from "next/image";
 import { AnimatePresence, motion } from "framer-motion";
 
+const useIsTabletOrSmaller = () => {
+  const [isTabletOrSmaller, setIsTabletOrSmaller] = useState(
+    window.innerWidth <= 768
+  );
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    const handleResize = () => setIsTabletOrSmaller(mediaQuery.matches);
+
+    mediaQuery.addEventListener("change", handleResize);
+    return () => mediaQuery.removeEventListener("change", handleResize);
+  }, []);
+
+  return isTabletOrSmaller;
+};
+
 const series = staticData;
 
 export default function Home() {
   const [view, setView] = useState<string>("home");
+  const [isGetStarted, setIsGetStarted] = useState(true);
+  const [isScaled, setIsScaled] = useState(true);
   const [selectedSize, setSelectedSize] = useState<watchSize>(
     series[0].sizes[1]
   );
@@ -34,6 +52,7 @@ export default function Home() {
       size: selectedSize,
     },
   ]);
+  const isTabletOrSmaller = useIsTabletOrSmaller();
 
   function selectSize(size: watchSize, band?: watchItem, caze?: watchItem) {
     setSelectedSize(size);
@@ -111,164 +130,229 @@ export default function Home() {
 
   return (
     <main className="typography-body">
+      <AnimatePresence>
+        {isGetStarted && (
+          <motion.div
+            key="getStarted"
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            variants={variants}
+            transition={{ duration: 0.5 }}
+            className="absolute top-36 left-1/4 transform -translate-x-1/4 z-100"
+          >
+            <h1
+              className="rf-designstudio-headline typography-headline-elevated"
+              id="rf-designstudio-a11yoverlaydesc"
+            >
+              <div role="text">
+                <div className="as-designstudio-intro-collectionname">
+                  Apple Watch Studio
+                </div>
+                <div className="as-designstudio-intro-casemsg">
+                  Choose a case.
+                </div>
+                <div className="as-designstudio-intro-bandmsg">
+                  Pick a band.
+                </div>
+                <div className="as-designstudio-intro-stylemsg">
+                  Create your own style.
+                </div>
+              </div>
+            </h1>
+            <button
+              className="px-4 py-2 typography-body bg-[#0071e3] text-white rounded-2xl mt-7"
+              onClick={() => {
+                setIsScaled(false);
+                setIsGetStarted(false);
+              }}
+            >
+              get started
+            </button>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <div className="flex justify-between h-[6.875rem]">
         <span className="px-[2.0625rem] py-[2.0625rem] w-40">
           <Image src="/watch.jpeg" alt="logo" width={90} height={90} />
         </span>
-        <Dropdown options={series.map((s) => s.name)} onSelect={setSeries} />
+        <span>
+          {!isGetStarted && (
+            <Dropdown
+              options={series.map((s) => s.name)}
+              onSelect={setSeries}
+            />
+          )}
+        </span>
         <span className="px-[1.375rem] py-[1.375rem] w-40">
-          <button className="button float-end px-4 py-2 typography-body bg-[#0071e3] text-white rounded-2xl">
-            Save
-          </button>
+          {!isGetStarted && (
+            <button className="button float-end px-4 py-2 typography-body bg-[#0071e3] text-white rounded-2xl">
+              Save
+            </button>
+          )}
         </span>
       </div>
-      <AnimatePresence mode="wait">
-        {view === "home" && (
-          <motion.div
-            key="home"
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            variants={variants}
-            transition={{ duration: 0.5 }}
-          >
-            <SizeCarousel
-              watches={watchList}
-              selectedSize={selectedSize}
-              onSelect={selectSize}
-            />
-          </motion.div>
-        )}
-        {view === "Size" && (
-          <motion.div
-            key="size"
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            variants={variants}
-            transition={{ duration: 0.5 }}
-          >
-            <SizeCarousel
-              watches={watchList}
-              selectedSize={selectedSize}
-              onSelect={selectSize}
-            />
-          </motion.div>
-        )}
-        {view === "Band" && (
-          <motion.div
-            key="band"
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            variants={variants}
-            transition={{ duration: 0.5 }}
-          >
-            <WatchCarousel
-              items={bands}
-              staticItem={selectedCase || cases[0]}
-              staticInFront={true}
-              onSelect={setSelectedBand}
-              initialSelectedItem={selectedBand || undefined}
-            />
-          </motion.div>
-        )}
-        {view === "Case" && (
-          <motion.div
-            key="case"
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            variants={variants}
-            transition={{ duration: 0.5 }}
-          >
-            <WatchCarousel
-              items={cases}
-              staticItem={selectedBand || bands[0]}
-              staticInFront={false}
-              onSelect={setSelectedCase}
-              initialSelectedItem={selectedCase || undefined}
-            />
-          </motion.div>
-        )}
-      </AnimatePresence>
-      <div className="text-center mt-[3rem]">
-        <div className="rf-designstudio-productinfo typography-body-reduced">
-          <div aria-live="polite" role="text">
-            <div className="rf-designstudio-productcollection typography-caption">
-              {selectedSeries.name}
-            </div>
-            <div className="rf-designstudio-producttitle typography-body-reduced">
-              {selectedSize.name} {selectedCase?.name} with {selectedBand?.name}
-            </div>
-            <div className="rf-designstudio-productprice typography-body-reduced">
-              <div className="rf-designstudio-pricepoint-fullPrice-comparative">
-                From{" "}
-                <span className="nowrap">
-                  ${selectedBand?.price + selectedCase?.price}
-                </span>
+      <motion.div
+        initial={{ scale: isTabletOrSmaller ? 1.5 : 2, translateY: 550 }}
+        animate={{
+          scale: isScaled ? (isTabletOrSmaller ? 1.5 : 2) : 1,
+          translateY: isScaled ? 550 : 0,
+        }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
+        <AnimatePresence mode="wait">
+          {view === "home" && (
+            <motion.div
+              key="home"
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={variants}
+              transition={{ duration: 0.5 }}
+            >
+              <SizeCarousel
+                watches={watchList}
+                selectedSize={selectedSize}
+                onSelect={selectSize}
+              />
+            </motion.div>
+          )}
+          {view === "Size" && (
+            <motion.div
+              key="size"
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={variants}
+              transition={{ duration: 0.5 }}
+            >
+              <SizeCarousel
+                watches={watchList}
+                selectedSize={selectedSize}
+                onSelect={selectSize}
+              />
+            </motion.div>
+          )}
+          {view === "Band" && (
+            <motion.div
+              key="band"
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={variants}
+              transition={{ duration: 0.5 }}
+            >
+              <WatchCarousel
+                items={bands}
+                staticItem={selectedCase || cases[0]}
+                staticInFront={true}
+                onSelect={setSelectedBand}
+                initialSelectedItem={selectedBand || undefined}
+              />
+            </motion.div>
+          )}
+          {view === "Case" && (
+            <motion.div
+              key="case"
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={variants}
+              transition={{ duration: 0.5 }}
+            >
+              <WatchCarousel
+                items={cases}
+                staticItem={selectedBand || bands[0]}
+                staticInFront={false}
+                onSelect={setSelectedCase}
+                initialSelectedItem={selectedCase || undefined}
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
+      {!isGetStarted && (
+        <div className="text-center mt-[3rem]">
+          <div className="rf-designstudio-productinfo typography-body-reduced">
+            <div aria-live="polite" role="text">
+              <div className="rf-designstudio-productcollection typography-caption">
+                {selectedSeries.name}
               </div>
-              <div className="rf-designstudio-pricepoint-acmiPrice-comparative"></div>
+              <div className="rf-designstudio-producttitle typography-body-reduced">
+                {selectedSize.name} {selectedCase?.name} with{" "}
+                {selectedBand?.name}
+              </div>
+              <div className="rf-designstudio-productprice typography-body-reduced">
+                <div className="rf-designstudio-pricepoint-fullPrice-comparative">
+                  From{" "}
+                  <span className="nowrap">
+                    ${selectedBand?.price + selectedCase?.price}
+                  </span>
+                </div>
+                <div className="rf-designstudio-pricepoint-acmiPrice-comparative"></div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="flex justify-center">
-        <div className="flex gap-4 my-16 overflow-x-auto max-w-full hide-scroll-bar">
-          <span
-            onClick={() => {
-              const watches: watch[] = [];
-              const bandName = removeLeadingNumbers(selectedBand.name);
-              const caseName = removeLeadingNumbers(selectedCase.name);
-              selectedSeries.sizes.forEach((size) => {
-                const band = size.bands.find(
-                  (b) => removeLeadingNumbers(b.name) === bandName
-                );
-                const casze = size.cases.find(
-                  (c) => removeLeadingNumbers(c.name) === caseName
-                );
-                if (band && casze) {
-                  watches.push({ case: casze, band: band, size: size });
+      )}
+      {!isGetStarted && (
+        <div className="flex justify-center">
+          <div className="flex gap-4 my-16 overflow-x-auto max-w-full hide-scroll-bar">
+            <span
+              onClick={() => {
+                const watches: watch[] = [];
+                const bandName = removeLeadingNumbers(selectedBand.name);
+                const caseName = removeLeadingNumbers(selectedCase.name);
+                selectedSeries.sizes.forEach((size) => {
+                  const band = size.bands.find(
+                    (b) => removeLeadingNumbers(b.name) === bandName
+                  );
+                  const casze = size.cases.find(
+                    (c) => removeLeadingNumbers(c.name) === caseName
+                  );
+                  if (band && casze) {
+                    watches.push({ case: casze, band: band, size: size });
+                  }
+                });
+                setWatchList(watches);
+                setView("Size");
+              }}
+            >
+              <ButtonList
+                items={
+                  view !== "Size"
+                    ? ["Size"]
+                    : selectedSeries.sizes.map((s) => s.name)
                 }
-              });
-              setWatchList(watches);
-              setView("Size");
-            }}
-          >
-            <ButtonList
-              items={
-                view !== "Size"
-                  ? ["Size"]
-                  : selectedSeries.sizes.map((s) => s.name)
-              }
-              heightLightItem={selectedSize.name}
-              typeChange={watchSizeChange}
-            ></ButtonList>
-          </span>
-          <span onClick={() => setView("Case")}>
-            <ButtonList
-              items={
-                view !== "Case"
-                  ? ["Case"]
-                  : [...new Set(cases.map((c) => c.type))]
-              }
-              heightLightItem={selectedCase.type}
-              typeChange={caseTypeChange}
-            ></ButtonList>
-          </span>
-          <span onClick={() => setView("Band")}>
-            <ButtonList
-              items={
-                view !== "Band"
-                  ? ["Band"]
-                  : [...new Set(bands.map((b) => b.type))]
-              }
-              heightLightItem={selectedBand.type}
-              typeChange={bandTypeChange}
-            ></ButtonList>
-          </span>
+                heightLightItem={selectedSize.name}
+                typeChange={watchSizeChange}
+              ></ButtonList>
+            </span>
+            <span onClick={() => setView("Case")}>
+              <ButtonList
+                items={
+                  view !== "Case"
+                    ? ["Case"]
+                    : [...new Set(cases.map((c) => c.type))]
+                }
+                heightLightItem={selectedCase.type}
+                typeChange={caseTypeChange}
+              ></ButtonList>
+            </span>
+            <span onClick={() => setView("Band")}>
+              <ButtonList
+                items={
+                  view !== "Band"
+                    ? ["Band"]
+                    : [...new Set(bands.map((b) => b.type))]
+                }
+                heightLightItem={selectedBand.type}
+                typeChange={bandTypeChange}
+              ></ButtonList>
+            </span>
+          </div>
         </div>
-      </div>
+      )}
     </main>
   );
 }
