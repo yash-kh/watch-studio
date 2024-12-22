b06a3abdc758f0ca1b85b33f6f61a6669c7ae618
yash-kh <yashkhatri2198@gmail.com>
2024-12-22 12:20:10 +0530

    button UI updated

--- components/WatchCarousel/WatchCarousel.tsx
+++ components/WatchCarousel/WatchCarousel.tsx
@@ -208,17 +208,35 @@ const WatchCarousel: React.FC<WatchCarouselProps> = ({
         />
       </div>
 
-      <div className="actions hidden">
-        <button className="prev" onClick={handlePrev}>
-          Prev
+      {!isTabletOrSmaller && currentCard > 0 && (
+        <button
+          className="prev bg-[#e8e8ed] p-1 rounded-full absolute top-1/2 left-2 z-50 transform -translate-y-1/2"
+          onClick={handlePrev}
+        >
+          <svg
+            className="w-6 h-6"
+            viewBox="8 5 19 25"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M21.559,12.062 L15.618,17.984 L21.5221,23.944 C22.105,24.533 22.1021,25.482 21.5131,26.065 C21.2211,26.355 20.8391,26.4999987 20.4571,26.4999987 C20.0711,26.4999987 19.6851,26.352 19.3921,26.056 L12.4351,19.034 C11.8531,18.446 11.8551,17.4999987 12.4411,16.916 L19.4411,9.938 C20.0261,9.353 20.9781,9.354 21.5621,9.941 C22.1471,10.528 22.1451,11.478 21.5591,12.062 L21.559,12.062 Z"></path>
+          </svg>
         </button>
-        <button className="next" onClick={handleNext}>
-          Next
+      )}
+
+      {!isTabletOrSmaller && currentCard < items.length - 1 && (
+        <button
+          className="next bg-[#e8e8ed] p-1 rounded-full absolute top-1/2 right-2 z-50 transform -translate-y-1/2"
+          onClick={handleNext}
+        >
+          <svg
+            className="w-6 h-6"
+            viewBox="8 5 19 25"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M23.5587,16.916 C24.1447,17.4999987 24.1467,18.446 23.5647,19.034 L16.6077,26.056 C16.3147,26.352 15.9287,26.4999987 15.5427,26.4999987 C15.1607,26.4999987 14.7787,26.355 14.4867,26.065 C13.8977,25.482 13.8947,24.533 14.4777,23.944 L20.3818,17.984 L14.4408,12.062 C13.8548,11.478 13.8528,10.5279 14.4378,9.941 C15.0218,9.354 15.9738,9.353 16.5588,9.938 L23.5588,16.916 L23.5587,16.916 Z"></path>
+          </svg>
         </button>
-        <div className="snapped-card">
-          <p>Snapped card: {currentCard}</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
